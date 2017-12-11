d05e163e03c04a91c19de2a3bd0d17e2d6addd02
fernandoMontes <fernando@element.in>
2017-12-11 22:47:40 +0100

    removed bower

---
+++ assets.js
@@ -0,0 +1,13 @@
+
+const CSS = [
+    'bootstrap/dist/css/bootstrap.css'
+];
+const JS = [
+    'angular/angular.js',
+    'angular-resource/angular-resource.js',
+    'angular-route/angular-route.js',
+    'bootstrap/dist/js/bootstrap.js'
+
+];
+
+module.exports = [...JS, ...CSS];
